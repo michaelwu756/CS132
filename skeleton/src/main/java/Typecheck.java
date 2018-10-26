6b2162f3ce4a3cfa4ac907ee04b85e982e347106
Michael Wu <cheeserules43@gmail.com>
2018-10-25 19:16:20 -0700

    implemented typechecking rule for goal

--- skeleton/src/main/java/Typecheck.java
+++ skeleton/src/main/java/Typecheck.java
@@ -2,16 +2,30 @@ import typecheck.Helper;
 import typecheck.TypeCheckVisitor;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Typecheck {
 
     public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        new MiniJavaParser(br);
+        StringBuilder contents = new StringBuilder();
+        String s;
+        try {
+            while ((s = br.readLine()) != null) {
+                contents.append(s);
+                contents.append("\n");
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        new MiniJavaParser(new ByteArrayInputStream(contents.toString().getBytes()));
         try {
             Helper.init(MiniJavaParser.Goal());
             TypeCheckVisitor visitor = new TypeCheckVisitor();
+            MiniJavaParser.ReInit(new ByteArrayInputStream(contents.toString().getBytes()));
             visitor.visit(MiniJavaParser.Goal());
             if (visitor.correctlyTypeChecks()) {
                 System.out.println("Program type checked successfully");
@@ -20,6 +34,7 @@ public class Typecheck {
             }
         } catch (ParseException e) {
             System.out.println("Parse error");
+            e.printStackTrace();
         }
     }
 
--- skeleton/src/main/java/typecheck/TypeCheckVisitor.java
+++ skeleton/src/main/java/typecheck/TypeCheckVisitor.java
@@ -1,13 +1,43 @@
 package typecheck;
 
-import syntaxtree.NodeToken;
+import syntaxtree.*;
 import visitor.DepthFirstVisitor;
+
+import java.util.*;
+
+import static typecheck.Helper.*;
 
 public class TypeCheckVisitor extends DepthFirstVisitor {
     private boolean typeChecks = true;
 
+    public void visit(Goal n) {
+        List<String> classNames = new ArrayList<>();
+        Set<LinkSetPair> fullLinkSet = new HashSet<>();
+        classNames.add(className(n.f0));
+        for (Enumeration<Node> e = n.f1.elements(); e.hasMoreElements(); ) {
+            Node next = e.nextElement();
+            if (next instanceof TypeDeclaration) {
+                Node classDeclarationNode = ((TypeDeclaration) next).f0.choice;
+                if (classDeclarationNode instanceof ClassDeclaration) {
+                    ClassDeclaration classDeclaration = (ClassDeclaration) classDeclarationNode;
+                    classNames.add(className(classDeclaration));
+                    fullLinkSet.addAll(linkSet(classDeclaration));
+                } else if (classDeclarationNode instanceof ClassExtendsDeclaration) {
+                    ClassExtendsDeclaration classExtendsDeclaration = (ClassExtendsDeclaration) classDeclarationNode;
+                    classNames.add(className(classExtendsDeclaration));
+                    fullLinkSet.addAll(linkSet(classExtendsDeclaration));
+                }
+            }
+        }
+        if (!distinct(classNames) || !acyclic(fullLinkSet)) {
+            typeChecks = false;
+        } else {
+            super.visit(n);
+        }
+    }
+
     public void visit(NodeToken n) {
-        System.out.print("\"" + n.toString() + "\" ");
+        System.err.print("\"" + n.toString() + "\" ");
     }
 
     public boolean correctlyTypeChecks() {
